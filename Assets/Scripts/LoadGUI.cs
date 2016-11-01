3a2d50b018e8da33b8bfaff4fd5c4ea0a9bbe5a8
naichilab <naichilab@live.jp>
2016-11-02 02:00:22 +0900

    初期コミット

---
+++ Assets/Scripts/LoadGUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGUI : MonoBehaviour
+{
+
+	public void OnGUI ()
+	{
+		if (GUI.Button (new Rect (300, 20, 100, 50), "Load")) {
+			string prefabPath = PlayerPrefs.GetString ("CUBE");
+			GameObject prefab = Resources.Load (prefabPath) as GameObject;
+			Instantiate (prefab, Vector3.zero, Quaternion.identity);
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/SaveGUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGUI : MonoBehaviour
+{
+
+	public void OnGUI ()
+	{
+		if (GUI.Button (new Rect (20, 20, 200, 50), "Save RedCube")) {
+			PlayerPrefs.SetString ("CUBE", "Cube/RedCube");
+		}
+		if (GUI.Button (new Rect (20, 80, 200, 50), "Save BlueCube")) {
+			PlayerPrefs.SetString ("CUBE", "Cube/BlueCube");
+		}
+		if (GUI.Button (new Rect (20, 140, 200, 50), "Save GreenCube")) {
+			PlayerPrefs.SetString ("CUBE", "Cube/GreenCube");
+		}
+		if (GUI.Button (new Rect (20, 200, 200, 50), "Save YellowCube")) {
+			PlayerPrefs.SetString ("CUBE", "Cube/YellowCube");
+		}
+	}
+
+}
